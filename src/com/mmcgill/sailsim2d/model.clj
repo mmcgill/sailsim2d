c3425ec1d1775b10abb5da845fb3535a408c4d68
Matt McGill <matt.mcgill@gmail.com>
2015-01-04 12:53:19 -0500

    working on rotation from rudder
    linear motion is disabled, because it was wrong

--- src/com/mmcgill/sailsim2d/model.clj
+++ src/com/mmcgill/sailsim2d/model.clj
@@ -10,11 +10,16 @@
 (defn boat
   "Return a new boat at the given position with default property values."
   [pos]
-  {:pos         pos         ; vector
-   :theta       0.0         ; angle, radians
-   :sail-theta  0.0         ; angle, radians
-   :v           [0.0 0.0]   ; vector
-   :mass        56.0        ; kg
+  {:sail-theta   0.0         ; angle, radians
+   :rudder-theta 0.0         ; angle, radians
+   :pos          pos         ; vector
+   :v            [0.0 0.0]   ; vector
+   :a            [0.0 0.0]   ; vector
+   :theta        0.0         ; angle, radians
+   :alpha        0.0         ; radians/sec^2
+   :omega        0.0         ; raians/sec
+   :mass         56.0        ; kg
+   :length       3.6         ; m
    })
 
 (defn game-state
@@ -48,26 +53,65 @@
 
 (defn hull-cda [theta]
   ;; TODO: Use interpolation and pre-calculated table for hull shape
-  (let [k (Math/cos (+ theta Math/PI))]
+  (let [k (Math/cos (+ (* 2 theta) Math/PI))]
     (+ 1.5 (* k (+ 2.2 (* 0.8 k))))))
 
 (defn sail-cda [theta]
   ;; TODO: Use interpolation and pre-calculated table for sail shape
   (* 4 (hull-cda theta)))
 
+(defn rudder-cda [theta]
+  (* 0.0625 (/ (+ (Math/cos (+ (* 2 theta) Math/PI)) 1) 2)))
+
+(defn moment-of-inertia
+  "Moment of inertia of a rod of length l and mass m, with COM at rod's center.
+  http://en.wikipedia.org/wiki/List_of_moments_of_inertia"
+  [l m]
+  (/ (* m l l) 12))
+
 (defn vadd [[x1 y1] [x2 y2]] [(+ x1 x2) (+ y1 y2)])
 (defn vsub [[x1 y1] [x2 y2]] [(- x1 x2) (- y1 y2)])
 (defn vmul [[x1 y1] [x2 y2]] [(* x1 x2) (* y1 y2)])
 (defn vdiv [[x1 y1] [x2 y2]] [(/ x1 x2) (/ y1 y2)])
+(defn vrot [[x y] theta]
+  (let [a (Math/sin theta), b (Math/cos theta)]
+    [(- (* x b) (* y a)), (+ (* x a) (* y b))]))
+(defn vangle [[x y]] (Math/atan2 y x))
+(defn todeg [rad] (Math/toDegrees rad))
+(defn vmag [[x y]] (Math/sqrt (+ (* x x) (* y y))))
 
-(defn tick-boat [{:keys [current] :as game-state} {:keys [mass pos theta v] :as boat}]
+(def rudder-coefficient "Scales the rudder torque"      0.05)
+(def rotation-damping   "Damps rotation of the boat"    1.5)
+
+(defn rudder-torque [r current rudder-theta]
+  (let [theta (- rudder-theta (vangle current))
+        speed (vmag current)]
+    (* r rudder-coefficient speed speed (+ (Math/sin theta) #_(Math/sin (* 3 theta))))))
+
+(defn tick-boat [{:keys [current] :as game-state}
+                 {:keys [mass length pos v theta omega rudder-theta] :as boat}]
   ;; TODO: compute net force and torque, then apply to
   ;; velocity and position
-  (let [f (compute-drag water-rho (vsub current v) theta hull-cda)
-        a (vdiv f [mass mass]) 
-        v (vadd v (vmul a [secs-per-tick secs-per-tick]))
+  (let [current (vsub current v)
+        tr (rudder-torque (/ length 2) current (+ theta rudder-theta))
+        td (* (- rotation-damping) omega (/ length 2))
+        t (+ tr td)
+        fh (compute-drag water-rho current theta hull-cda)
+        ;_ (prn :fh fh)
+        f fh
+
+        ;; rotational dynamics
+        alpha (/ t (moment-of-inertia length mass))
+        omega (+ omega (* secs-per-tick alpha))
+        theta (+ theta (* secs-per-tick omega))
+
+        ;; linear dynamics
+        ;;a (vdiv f [mass mass])
+        ;;v (vadd v (vmul a [secs-per-tick secs-per-tick]))
         pos (vadd pos (vmul v [secs-per-tick secs-per-tick]))]
-    (assoc boat :pos pos :v v)))
+    (assoc boat
+           ;;:pos pos :v v :a a
+           :theta theta :omega omega :alpha alpha)))
 
 (defn tick-boats [game-state boats]
   (reduce-kv #(assoc %1 %2 (tick-boat game-state %3)) {} boats))
